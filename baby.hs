29d01cad3843b1f63ac9b4e40199887d9276fc89
David <davidhiggins1712@gmail.com>
2016-12-05 23:51:11 +0000

    Types

--- baby.hs
+++ baby.hs
@@ -12,6 +12,19 @@ length' xs = sum [ 1| _ <- xs]
 
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
+-- Chapter 2 - Types and Typeclasses --
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
 {-
 To run:
  	go to folder of file
