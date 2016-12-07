03846eedc7518b49dfcde3479cad7c1c6b15ea9c
David <davidhiggins1712@gmail.com>
2016-12-07 22:44:44 +0000

    mm

--- chapter1.hs
+++ chapter1.hs
@@ -20,11 +20,6 @@ boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [ 1| _ <- xs]
 
-removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
-
-
-
--- fromIntegral (length [1,2,3,4]) + 3.2
 
 
 
--- chapter3.hs
+++ chapter3.hs
@@ -1,43 +1,16 @@
--- Chapter 3
+-- Chapter 3 - Types and Typeclasses --
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
-lucky :: (Integral a) => a -> String
-lucky 7 = "LUCKY NUMBER SEVEN!"
-lucky x = "Sorry you're out of luck"
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
 
-sayMe :: (Integral a) => a -> String  
-sayMe 1 = "One!"  
-sayMe 2 = "Two!"  
-sayMe 3 = "Three!"  
-sayMe 4 = "Four!"  
-sayMe 5 = "Five!"  
-sayMe x = "Not between 1 and 5" 
+factorial :: Integer -> Integer
+factorial n = product [1..n]
 
-factorial' :: (Integral a) => a -> a
-factorial' 0 = 1
-factorial' n = n * factorial' (n-1)
+circumference :: Float -> Float
+circumference r = 2 * pi * r
 
-addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
-addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
 
-first :: (a,b,c) -> a
-first  (x, _, _) = x
-
-second :: (a,b,c) -> b
-second ( _, y, _) = y
-
-third :: (a, b, c) -> c
-third ( _, _, z) = z
-
-head' :: [a] -> a
-head' [] = error "Can't call head on an empty list, dummy"
-head' (x:_) = x
-
-tell :: (Show a) => [a] -> String
-tell [] = "This list is empty"
-tell (x:[]) = "The list has one element: " ++ show x
-tell (x:y:[]) = "The list has two elements: " ++ show ++ " and " ++ show y
-tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
-
-length' :: (Num b) => [a] -> b
-length' [] = 0
-length' (_:xs) = 1 + length' xs
+-- fromIntegral (length [1,2,3,4]) + 3.2
